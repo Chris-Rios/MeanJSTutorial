2fabf33f717809b75eb53288ea0a881c2092a200
Christopher Rios <rioschm@gmail.com>
2015-06-29 12:01:27 -0400

    Flesh out need schema

--- volunteer/app/models/need.server.model.js
+++ volunteer/app/models/need.server.model.js
@@ -4,26 +4,60 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Need Schema
  */
 var NeedSchema = new Schema({
-	name: {
-		type: String,
-		default: '',
-		required: 'Please fill Need name',
-		trim: true
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	user: {
-		type: Schema.ObjectId,
-		ref: 'User'
-	}
+    title: {
+        type: String,
+        default: '',
+        required: 'Please provide a title',
+        trim: true
+    },
+    description: {
+        type: String,
+        default: '',
+        required: 'Please provide a description',
+        trim: true
+    },
+    organization: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    location: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    startDate: {
+        type: Date
+    },
+    endDate: {
+        type: Date
+    },
+    numberNeeded: {
+        type: Number,
+        default: 1
+    },
+    volunteers: [{
+        user: Schema.ObjectId,
+
+    }],
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+    createdBy: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Need', NeedSchema);
